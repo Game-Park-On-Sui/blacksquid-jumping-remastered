c411e548c9a3fd8b3e8c8c9db4190155d579b620
Zcy <zcy_chao1024@163.com>
2025-04-10 16:40:37 +0800

    login page

--- cocos/assets/Scripts/UIManager.ts
+++ cocos/assets/Scripts/UIManager.ts
@@ -6,11 +6,18 @@ const {ccclass, property} = _decorator;
 @ccclass('UIManager')
 export class UIManager extends Component {
     @property({type: Node})
+    login: Node = null;
+    @property({type: Node})
     startButton: Node = null;
     @property({type: Node})
     chooseGame: Node = null;
     @property({type: SpikesManager})
     spikesManager: SpikesManager = null;
+
+    handleClickLogin() {
+        this.login.active = false;
+        this.startButton.active = true;
+    }
 
     handleClickStart() {
         this.startButton.active = false;
